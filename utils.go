7d103e93441c5e1cb4601add3d306840a354d4a8
Ollivier Robert <roberto@keltia.net>
2018-08-09 17:16:26 +0200

    Our usual utilities.

---
+++ utils.go
@@ -0,0 +1,28 @@
+// utils.go
+//
+// Copyright 2018 © by Ollivier Robert <roberto@keltia.net>
+
+package observatory
+
+import "log"
+
+// debug displays only if fDebug is set
+func (c *Client) debug(str string, a ...interface{}) {
+	if c.level >= 2 {
+		log.Printf(str, a...)
+	}
+}
+
+// debug displays only if fVerbose is set
+func (c *Client) verbose(str string, a ...interface{}) {
+	if c.level >= 1 {
+		log.Printf(str, a...)
+	}
+}
+
+// debug displays only if fVerbose is set
+func (c *Client) fatal(str string, a ...interface{}) {
+	if c.level >= 1 {
+		log.Fatalf(str, a...)
+	}
+}
