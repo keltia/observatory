5581f402aca1f2aed637acb999b642357aa7e5be
Ollivier Robert <roberto@keltia.net>
2019-09-05 14:33:44 +0200

    Welcome 1.3.1 with looping fixed when error.

--- mozilla.go
+++ mozilla.go
@@ -23,7 +23,7 @@ const (
 	DefaultRetry = 5
 
 	// MyVersion is the API version
-	MyVersion = "1.3.0"
+	MyVersion = "1.3.1"
 
 	// MyName is the name used for the configuration
 	MyName = "observatory"
